70a75d324fc82cd9725bd1dc11f1c61be5f1de5b
TylerBiggert <TylerB.Other@gmail.com>
2019-09-05 05:39:53 -0400

    Nav component styling
    -styled nav bar

---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+	return (
+		<nav>
+			<p className='f3 link underline pa3 pointer fr w-20'>Sign Out</p>
+		</nav>
+	);
+}
+
+export default Navigation;
